275ae7d5cf2f70c71b242edcee46c53d078beb90
altus34 <altus34@gmail.com>
2013-04-24 07:24:35 -0400

    Improve components tests

---
+++ src/main/java/org/testatoo/core/component/ComboBox.java
@@ -0,0 +1,11 @@
+package org.testatoo.core.component;
+
+/**
+ * @author David Avenante (d.avenante@gmail.com)
+ */
+public class ComboBox extends Component {
+
+    public ComboBox(String id) {
+        super(id);
+    }
+}
---
+++ src/main/java/org/testatoo/core/component/DropDown.java
@@ -0,0 +1,11 @@
+package org.testatoo.core.component;
+
+/**
+ * @author David Avenante (d.avenante@gmail.com)
+ */
+public class DropDown extends Component {
+
+    public DropDown(String id) {
+        super(id);
+    }
+}
