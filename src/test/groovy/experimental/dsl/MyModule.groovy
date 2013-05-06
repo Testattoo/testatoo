d937318e09003f47d1a683ed45c0d375d27c474f
mathieucarbou <mathieu.carbou@gmail.com>
2013-05-06 00:39:04 -0400

    licenses

--- src/test/groovy/experimental/dsl/MyModule.groovy
+++ src/test/groovy/experimental/dsl/MyModule.groovy
@@ -16,6 +16,8 @@
 package experimental.dsl
 
 import org.testatoo.config.AbstractTestatooModule
+import org.testatoo.config.Scope
+import org.testatoo.config.cartridge.TestatooCartridge
 
 /**
  * @author Mathieu Carbou (mathieu.carbou@gmail.com)
@@ -23,9 +25,32 @@ import org.testatoo.config.AbstractTestatooModule
  */
 class MyModule extends AbstractTestatooModule {
 
+    static int seleniumPort = -1;
+
     @Override
     protected void configure() {
 
+        if (seleniumPort == -1) {
+            seleniumPort = findFreePort();
+        }
 
+        seleniumServers().registerProvider(createSeleniumServer()
+            .port(4444)
+            .useSingleWindow(true)
+            .build())
+            .scope(Scope.TEST_CLASS);
+
+        seleniumSessions()
+            .registerProvider(createSeleniumSession()
+            .website("http://localhost:8080")
+            .browser("*googlechrome")
+            .serverHost("localhost")
+            .serverPort(4444)
+            .build())
+            .scope(Scope.TEST_CLASS)
+            .withTimeout(5000)
+            .inCartridge(TestatooCartridge.HTML4);
+
+        useAnnotations();
     }
 }
