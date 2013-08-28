2bf7da3033a617c4193bc29a00583559a29b9b50
mathieucarbou <mathieu.carbou@gmail.com>
2013-08-28 14:43:03 -0400

    improve test webapp

---
+++ testatoo-tests/webapp/js/app.js
@@ -0,0 +1,15 @@
+(function ($) {
+
+    $(function () {
+
+        $('#add-message').on('click', function () {
+            setTimeout(function () {
+                $('#messages ol').append('<li>' + new Date() + ' - a message</li>');
+                $('#add-message').prop('disabled', false);
+            }, 1000);
+            $('#add-message').prop('disabled', true);
+        });
+
+    });
+
+}(jQuery));
