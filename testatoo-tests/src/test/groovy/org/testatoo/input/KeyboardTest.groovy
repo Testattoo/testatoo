748c81fef1f9ebc24f441411d9f00b94b4c94475
altus34 <d.avenante@gmail.com>
2014-02-02 21:23:36 -0500

    Clean keyboard test

--- testatoo-tests/src/test/groovy/org/testatoo/input/KeyboardTest.groovy
+++ testatoo-tests/src/test/groovy/org/testatoo/input/KeyboardTest.groovy
@@ -9,50 +9,11 @@ import org.openqa.selenium.firefox.FirefoxDriver
 import org.testatoo.core.Testatoo
 import org.testatoo.core.evaluator.KeyboardAction
 import org.testatoo.core.evaluator.webdriver.WebDriverEvaluator
-import org.testatoo.core.input.Keyboard
 
-import static org.testatoo.core.Testatoo.$
-import static org.testatoo.core.Testatoo.assertThat
-import static org.testatoo.core.Testatoo.getEvaluator
-import static org.testatoo.core.Testatoo.open
-import static org.testatoo.core.input.Keyboard.press
-import static org.testatoo.core.input.Keyboard.release
-import static org.testatoo.core.input.Keyboard.type
-import static org.testatoo.core.input.Keys.ADD
-import static org.testatoo.core.input.Keys.BACK_SPACE
-import static org.testatoo.core.input.Keys.DELETE
-import static org.testatoo.core.input.Keys.DIVIDE
-import static org.testatoo.core.input.Keys.DOWN
-import static org.testatoo.core.input.Keys.END
-import static org.testatoo.core.input.Keys.EQUALS
-import static org.testatoo.core.input.Keys.ESCAPE
-import static org.testatoo.core.input.Keys.F1
-import static org.testatoo.core.input.Keys.F10
-import static org.testatoo.core.input.Keys.F11
-import static org.testatoo.core.input.Keys.F12
-import static org.testatoo.core.input.Keys.F2
-import static org.testatoo.core.input.Keys.F3
-import static org.testatoo.core.input.Keys.F4
-import static org.testatoo.core.input.Keys.F5
-import static org.testatoo.core.input.Keys.F6
-import static org.testatoo.core.input.Keys.F7
-import static org.testatoo.core.input.Keys.F8
-import static org.testatoo.core.input.Keys.F9
-import static org.testatoo.core.input.Keys.HOME
-import static org.testatoo.core.input.Keys.INSERT
-import static org.testatoo.core.input.Keys.LEFT
-import static org.testatoo.core.input.Keys.MULTIPLY
-import static org.testatoo.core.input.Keys.PAGE_DOWN
-import static org.testatoo.core.input.Keys.PAGE_UP
-import static org.testatoo.core.input.Keys.RETURN
-import static org.testatoo.core.input.Keys.RIGHT
-import static org.testatoo.core.input.Keys.SPACE
-import static org.testatoo.core.input.Keys.SUBTRACT
-import static org.testatoo.core.input.Keys.TAB
-import static org.testatoo.core.input.Keys.UP
-import static org.testatoo.core.input.KeysModifier.ALT
-import static org.testatoo.core.input.KeysModifier.CONTROL
-import static org.testatoo.core.input.KeysModifier.SHIFT
+import static org.testatoo.core.Testatoo.*
+import static org.testatoo.core.input.Keyboard.*
+import static org.testatoo.core.input.Keys.*
+import static org.testatoo.core.input.KeysModifier.*
 import static org.testatoo.core.state.States.available
 import static org.testatoo.core.state.States.getMissing
 
@@ -64,263 +25,109 @@ class KeyboardTest {
 
     @BeforeClass
     public static void setup() {
-        Testatoo.evaluator =  new WebDriverEvaluator(new FirefoxDriver())
+        Testatoo.evaluator = new WebDriverEvaluator(new FirefoxDriver())
         open('http://localhost:8080/keyboard.html')
     }
-    @AfterClass public static void tearDown() { evaluator.close() }
+
+    @AfterClass
+    public static void tearDown() { evaluator.close() }
 
     @Test
     public void test_letters_on_keyboard_() {
-        assertThat $('#span_a') is missing
-        assertThat $('#span_b') is missing
-        assertThat $('#span_c') is missing
-        assertThat $('#span_d') is missing
-        assertThat $('#span_e') is missing
-        assertThat $('#span_f') is missing
-        assertThat $('#span_g') is missing
-        assertThat $('#span_h') is missing
-        assertThat $('#span_i') is missing
-        assertThat $('#span_j') is missing
-        assertThat $('#span_k') is missing
-        assertThat $('#span_l') is missing
-        assertThat $('#span_m') is missing
-        assertThat $('#span_n') is missing
-        assertThat $('#span_o') is missing
-        assertThat $('#span_p') is missing
-        assertThat $('#span_q') is missing
-        assertThat $('#span_r') is missing
-        assertThat $('#span_s') is missing
-        assertThat $('#span_t') is missing
-        assertThat $('#span_u') is missing
-        assertThat $('#span_v') is missing
-        assertThat $('#span_w') is missing
-        assertThat $('#span_x') is missing
-        assertThat $('#span_y') is missing
-        assertThat $('#span_z') is missing
-
-        type('a')
-        type('b')
-        type('c')
-        type('d')
-        type('e')
-        type('f')
-        type('g')
-        type('h')
-        type('i')
-        type('j')
-        type('k')
-        type('l')
-        type('m')
-        type('n')
-        type('o')
-        type('p')
-        type('q')
-        type('r')
-        type('s')
-        type('t')
-        type('u')
-        type('v')
-        type('w')
-        type('x')
-        type('y')
-        type('z')
-
-        assertThat $('#span_a') is available
-        assertThat $('#span_b') is available
-        assertThat $('#span_c') is available
-        assertThat $('#span_d') is available
-        assertThat $('#span_e') is available
-        assertThat $('#span_f') is available
-        assertThat $('#span_g') is available
-        assertThat $('#span_h') is available
-        assertThat $('#span_i') is available
-        assertThat $('#span_j') is available
-        assertThat $('#span_k') is available
-        assertThat $('#span_l') is available
-        assertThat $('#span_m') is available
-        assertThat $('#span_n') is available
-        assertThat $('#span_o') is available
-        assertThat $('#span_p') is available
-        assertThat $('#span_q') is available
-        assertThat $('#span_r') is available
-        assertThat $('#span_s') is available
-        assertThat $('#span_t') is available
-        assertThat $('#span_u') is available
-        assertThat $('#span_v') is available
-        assertThat $('#span_w') is available
-        assertThat $('#span_x') is available
-        assertThat $('#span_y') is available
-        assertThat $('#span_z') is available
+        [
+            '#span_a': 'a',
+            '#span_b': 'b',
+            '#span_c': 'c',
+            '#span_d': 'd',
+            '#span_e': 'e',
+            '#span_f': 'f',
+            '#span_g': 'g',
+            '#span_h': 'h',
+            '#span_i': 'i',
+            '#span_j': 'j',
+            '#span_k': 'k',
+            '#span_l': 'l',
+            '#span_m': 'm',
+            '#span_n': 'n',
+            '#span_o': 'o',
+            '#span_p': 'p',
+            '#span_q': 'q',
+            '#span_r': 'r',
+            '#span_s': 's',
+            '#span_t': 't',
+            '#span_u': 'u',
+            '#span_v': 'v',
+            '#span_w': 'w',
+            '#span_x': 'x',
+            '#span_y': 'y',
+            '#span_z': 'z'
+        ].each { k, v ->
+            assertThat $(k) is missing
+            type v
+            assertThat $(k) is available
+        }
     }
 
     @Test
     public void test_number() {
-        assertThat $('#span_1') is missing
-        assertThat $('#span_2') is missing
-        assertThat $('#span_3') is missing
-        assertThat $('#span_4') is missing
-        assertThat $('#span_5') is missing
-        assertThat $('#span_6') is missing
-        assertThat $('#span_7') is missing
-        assertThat $('#span_8') is missing
-        assertThat $('#span_9') is missing
-        assertThat $('#span_0') is missing
-
-        type('1')
-        type('2')
-        type('3')
-        type('4')
-        type('5')
-        type('6')
-        type('7')
-        type('8')
-        type('9')
-        type('0')
-
-        assertThat $('#span_1') is available
-        assertThat $('#span_2') is available
-        assertThat $('#span_3') is available
-        assertThat $('#span_4') is available
-        assertThat $('#span_5') is available
-        assertThat $('#span_6') is available
-        assertThat $('#span_7') is available
-        assertThat $('#span_8') is available
-        assertThat $('#span_9') is available
-        assertThat $('#span_0') is available
+        [
+            '#span_1': '1',
+            '#span_2': '2',
+            '#span_3': '3',
+            '#span_4': '4',
+            '#span_5': '5',
+            '#span_6': '6',
+            '#span_7': '7',
+            '#span_8': '8',
+            '#span_9': '9',
+            '#span_0': '0'
+        ].each { k, v ->
+            assertThat $(k) is missing
+            type v
+            assertThat $(k) is available
+        }
     }
 
     @Test
     public void test_key() {
-        /*[
-            '#span_esc' : ESCAPE,
-            '#span_f1': F1
-
-        ].each {k, v->
+        [
+            '#span_esc': ESCAPE,
+            '#span_f1': F1,
+            '#span_f2': F2,
+            '#span_f3': F3,
+            '#span_f4': F4,
+            '#span_f5': F5,
+            '#span_f6': F6,
+            '#span_f7': F7,
+            '#span_f8': F8,
+            '#span_f9': F9,
+            '#span_f10': F10,
+            '#span_f11': F11,
+            '#span_f12': F12,
+            '#span_insert': INSERT,
+            '#span_del': DELETE,
+            '#span_pageup': PAGE_UP,
+            '#span_pagedown': PAGE_DOWN,
+            '#span_home': HOME,
+            '#span_end': END,
+            '#span_backspace': BACK_SPACE,
+            '#span_divide': DIVIDE,
+            '#span_multiply': MULTIPLY,
+            '#span_substract': SUBTRACT,
+            '#span_add': ADD,
+            '#span_equals': EQUALS,
+            '#span_tab': TAB,
+            '#span_return': RETURN,
+            '#span_space': SPACE,
+            '#span_left': LEFT,
+            '#span_up': UP,
+            '#span_right': RIGHT,
+            '#span_down': DOWN
+        ].each { k, v ->
             assertThat $(k) is missing
             type v
             assertThat $(k) is available
-        }*/
-
-        assertThat $('#span_esc') is missing
-
-        assertThat $('#span_f1') is missing
-        assertThat $('#span_f2') is missing
-        assertThat $('#span_f3') is missing
-        assertThat $('#span_f4') is missing
-        assertThat $('#span_f5') is missing
-        assertThat $('#span_f6') is missing
-        assertThat $('#span_f7') is missing
-        assertThat $('#span_f8') is missing
-        assertThat $('#span_f9') is missing
-        assertThat $('#span_f10') is missing
-        assertThat $('#span_f11') is missing
-        assertThat $('#span_f12') is missing
-
-        assertThat $('#span_insert') is missing
-        assertThat $('#span_del') is missing
-        assertThat $('#span_pageup') is missing
-        assertThat $('#span_pagedown') is missing
-
-        assertThat $('#span_home') is missing
-        assertThat $('#span_end') is missing
-        assertThat $('#span_backspace') is missing
-
-        assertThat $('#span_divide') is missing
-        assertThat $('#span_multiply') is missing
-        assertThat $('#span_substract') is missing
-        assertThat $('#span_add') is missing
-        assertThat $('#span_equals') is missing
-
-        assertThat $('#span_tab') is missing
-        assertThat $('#span_return') is missing
-        assertThat $('#span_space') is missing
-
-        assertThat $('#span_left') is missing
-        assertThat $('#span_up') is missing
-        assertThat $('#span_right') is missing
-        assertThat $('#span_down') is missing
-
-        Keyboard.releaseAll()
-
-        type(ESCAPE)
-
-        type(F1)
-        type(F2)
-        type(F3)
-        type(F4)
-        type(F5)
-        type(F6)
-        type(F7)
-        type(F8)
-        type(F9)
-        type(F10)
-        type(F11)
-        type(F12)
-
-        type(INSERT)
-        type(DELETE)
-        type(PAGE_UP)
-        type(PAGE_DOWN)
-
-        type(HOME)
-        type(END)
-        type(BACK_SPACE)
-
-        type(MULTIPLY)
-        type(DIVIDE)
-        type(SUBTRACT)
-        type(ADD)
-        type(EQUALS)
-
-        type(TAB)
-        type(RETURN)
-        type(SPACE)
-
-        type(LEFT)
-        type(UP)
-        type(RIGHT)
-        type(DOWN)
-
-        sleep(5000)
-
-        assertThat $('#span_esc') is available
-
-        assertThat $('#span_f1') is available
-        assertThat $('#span_f2') is available
-        assertThat $('#span_f3') is available
-        assertThat $('#span_f4') is available
-        assertThat $('#span_f5') is available
-        assertThat $('#span_f6') is available
-        assertThat $('#span_f7') is available
-        assertThat $('#span_f8') is available
-        assertThat $('#span_f9') is available
-        assertThat $('#span_f10') is available
-        assertThat $('#span_f11') is available
-        assertThat $('#span_f12') is available
-
-        assertThat $('#span_insert') is available
-        assertThat $('#span_del') is available
-        assertThat $('#span_pageup') is available
-        assertThat $('#span_pagedown') is available
-
-        assertThat $('#span_home') is available
-        assertThat $('#span_end') is available
-        assertThat $('#span_backspace') is available
-
-        assertThat $('#span_divide') is available
-        assertThat $('#span_multiply') is available
-        assertThat $('#span_substract') is available
-        assertThat $('#span_add') is available
-        assertThat $('#span_equals') is available
-
-        assertThat $('#span_tab') is available
-        assertThat $('#span_return') is available
-        assertThat $('#span_space') is available
-
-        assertThat $('#span_left') is available
-        assertThat $('#span_up') is available
-        assertThat $('#span_right') is available
-        assertThat $('#span_down') is available
+        }
     }
 
     @Test
