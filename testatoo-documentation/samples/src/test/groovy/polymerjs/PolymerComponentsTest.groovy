e7d615783f37a6a6691c0b7564aa77f09b9ae464
altus34 <d.avenante@gmail.com>
2014-09-01 13:50:43 -0400

    Fix Polymer test

--- testatoo-documentation/samples/src/test/groovy/polymerjs/PolymerComponentsTest.groovy
+++ testatoo-documentation/samples/src/test/groovy/polymerjs/PolymerComponentsTest.groovy
@@ -56,14 +56,13 @@ class PolymerComponentsTest {
 
         clickOn tab_panel.tabs[1]
 
-        waitUntil { tab_panel.tabs[1].is(selected) }
-
+        waitUntil 10.seconds, { tab_panel.tabs[1].is(selected) }
         assertThat tab_panel.tabs[0] is unSelected
         assertThat tab_panel.tabs[2] is unSelected
 
         clickOn tab_panel.tabs[2]
 
-        waitUntil { tab_panel.tabs[2].is(selected) }
+        waitUntil 10.seconds, { tab_panel.tabs[2].is(selected) }
 
         assertThat tab_panel.tabs[0] is unSelected
         assertThat tab_panel.tabs[1] is unSelected
