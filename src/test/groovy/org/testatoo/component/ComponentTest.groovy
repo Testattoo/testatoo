2ec7ca54feb2ae61edc464aa440860d951c955e6
altus34 <altus34@gmail.com>
2015-06-19 11:22:54 -0400

    Update components definitions

--- src/test/groovy/org/testatoo/component/ComponentTest.groovy
+++ src/test/groovy/org/testatoo/component/ComponentTest.groovy
@@ -24,13 +24,19 @@ import org.openqa.selenium.firefox.FirefoxDriver
 import org.testatoo.core.IdentifiedByJs
 import org.testatoo.core.Testatoo
 import org.testatoo.core.component.Button
+import org.testatoo.core.component.Checkbox
+import org.testatoo.core.component.Component
+import org.testatoo.core.component.ComponentException
 import org.testatoo.core.component.Paragraph
 import org.testatoo.core.component.Radio
 import org.testatoo.core.component.Section
 import org.testatoo.core.evaluator.webdriver.WebDriverEvaluator
+import org.testatoo.core.state.States
 
+import static org.junit.Assert.fail
 import static org.testatoo.core.Testatoo.*
 import static org.testatoo.core.property.Properties.text
+import static org.testatoo.core.state.States.enabled
 
 /**
  * @author David Avenante (d.avenante@gmail.com)
@@ -41,13 +47,41 @@ class ComponentTest {
     @BeforeClass
     public static void setup() {
         Testatoo.evaluator = new WebDriverEvaluator(new FirefoxDriver())
-        evaluator.registerScripts(this.getClass().getResourceAsStream('/inheritance.js').text)
-
         open 'http://localhost:8080/components.html'
     }
 
     @AfterClass
     public static void tearDown() { evaluator.close() }
+
+    @Test
+    public void should_identify_component_by_css() {
+
+    }
+
+    @Test
+    public void should_identify_component_by_js() {
+
+    }
+
+    @Test
+    public void component_without_identifier_fail() {
+        try {
+            ($('#button') as CustomButton).should { be enabled }
+            fail()
+        } catch (e) {
+            assert e instanceof ComponentException
+            assert e.message == "Missing @Identifier annotation on type $CustomButton.name"
+        }
+
+    }
+
+    // custom component without identifier
+
+    @Test
+    public void on_bad_component_definition_an_error_it_thrown() {
+//        Checkbox radio_1 = $('#radio') as Radio
+
+    }
 
     @Test
     public void should_evaluate_component_equality() {
@@ -71,56 +105,62 @@ class ComponentTest {
     }
 
     @Test
-    public void should_be_able_to_manage_inheritance() {
-        Button button = $('#btn_default') as Button
-        button.should { have text('Default') }
-
-        PrimaryButton primary_button = $('#btn_primary') as PrimaryButton
-        primary_button.should { have text('Primary') }
-
-        SuccessButton success_button = $('#btn_success') as SuccessButton
-        success_button.should { have text('Success') }
-
-        InfoButton info_button = $('#btn_info') as InfoButton
-        info_button.should { have text('Info') }
-
-        WarningButton warning_button = $('#btn_warning') as WarningButton
-        warning_button.should { have text('Warning') }
-
-        DangerButton danger_button = $('#btn_danger') as DangerButton
-        danger_button.should { have text('Danger') }
-
-        Button button_1 = $('#btn_primary') as Button
-        button_1.should { have text('Primary') }
-        // TODO update test on inheritance
-//        try {
-//            primary_button = $('#btn_warning') as PrimaryButton
-//            primary_button.should { have text('Primary') }
-//            fail()
-//        } catch (Exception e) {
-//            assert e.message == "Expected Text 'Primary' but was 'Warning'"
-//        }
-    }
-
-    @Test
     public void the_hashCode_of_a_component_is_based_on_its_id() {
         Radio radio_1 = $('#radio') as Radio
         assert radio_1.hashCode() == radio_1.id.hashCode()
     }
 
-    @IdentifiedByJs("it.is('button') && it.hasClass('btn-primary')")
-    private class PrimaryButton extends Button {}
+//    @Test
+//    public void should_be_able_to_manage_inheritance() {
+//        Button button = $('#btn_default') as Button
+//        button.should { have text('Default') }
+//
+//        PrimaryButton primary_button = $('#btn_primary') as PrimaryButton
+//        primary_button.should { have text('Primary') }
+//
+//        SuccessButton success_button = $('#btn_success') as SuccessButton
+//        success_button.should { have text('Success') }
+//
+//        InfoButton info_button = $('#btn_info') as InfoButton
+//        info_button.should { have text('Info') }
+//
+//        WarningButton warning_button = $('#btn_warning') as WarningButton
+//        warning_button.should { have text('Warning') }
+//
+//        DangerButton danger_button = $('#btn_danger') as DangerButton
+//        danger_button.should { have text('Danger') }
+//
+//        Button button_1 = $('#btn_primary') as Button
+//        button_1.should { have text('Primary') }
+//        // TODO update test on inheritance
+////        try {
+////            primary_button = $('#btn_warning') as PrimaryButton
+////            primary_button.should { have text('Primary') }
+////            fail()
+////        } catch (Exception e) {
+////            assert e.message == "Expected Text 'Primary' but was 'Warning'"
+////        }
+//    }
 
-    @IdentifiedByJs("it.is('button') && it.hasClass('btn-success')")
-    private class SuccessButton extends  Button {}
 
-    @IdentifiedByJs("it.is('button') && it.hasClass('btn-info')")
-    private class InfoButton extends  Button {}
 
-    @IdentifiedByJs("it.is('button') && it.hasClass('btn-warning')")
-    private class WarningButton extends  Button {}
 
-    @IdentifiedByJs("it.is('button') && it.hasClass('btn-danger')")
-    private class DangerButton extends  Button {}
+//    @IdentifiedByJs("it.is('button') && it.hasClass('btn-primary')")
+//    private class PrimaryButton extends Button {}
+//
+//    @IdentifiedByJs("it.is('button') && it.hasClass('btn-success')")
+//    private class SuccessButton extends  Button {}
+//
+//    @IdentifiedByJs("it.is('button') && it.hasClass('btn-info')")
+//    private class InfoButton extends  Button {}
+//
+//    @IdentifiedByJs("it.is('button') && it.hasClass('btn-warning')")
+//    private class WarningButton extends  Button {}
+//
+//    @IdentifiedByJs("it.is('button') && it.hasClass('btn-danger')")
+//    private class DangerButton extends  Button {}
+
+    private class CustomButton extends Button {}
+
 
 }
